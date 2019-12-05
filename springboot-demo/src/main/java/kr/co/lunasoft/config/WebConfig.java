a80d60fab4cbd519a4858c6ec2cfd20c7b5bfc43
딘 <jhlee@lunasoft.co.kr>
2019-12-05 18:42:29 +0900

    apply swagger

---
+++ springboot-demo/src/main/java/kr/co/lunasoft/config/WebConfig.java
@@ -0,0 +1,20 @@
+package kr.co.lunasoft.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+@Import(SwaggerConfig.class)
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+
+}
