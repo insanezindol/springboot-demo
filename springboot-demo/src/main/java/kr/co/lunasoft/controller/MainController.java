0b36fb309f86ebad4094260cb042f0940b04ccc2
딘 <jhlee@lunasoft.co.kr>
2019-12-10 11:50:55 +0900

    remove unused import

--- springboot-demo/src/main/java/kr/co/lunasoft/controller/MainController.java
+++ springboot-demo/src/main/java/kr/co/lunasoft/controller/MainController.java
@@ -11,11 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
-import kr.co.lunasoft.model.ResponseInfo;
 import kr.co.lunasoft.util.HttpConnectionUtil;
 import lombok.extern.slf4j.Slf4j;
 import springfox.documentation.annotations.ApiIgnore;
