41ede84812f3c9bc49850c8b65c9ca8fad5416aa
딘 <jhlee@lunasoft.co.kr>
2019-12-11 14:57:42 +0900

    update jpa module

--- springboot-demo/src/main/java/kr/co/lunasoft/model/TopicInfo.java
+++ springboot-demo/src/main/java/kr/co/lunasoft/model/TopicInfo.java
@@ -11,25 +11,32 @@ import javax.persistence.Table;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Data;
 
+@ApiModel(value="TopicInfo", description="MySQL Topic Table Object")
 @Data
 @Entity
 @Table(name="topic")
 public class TopicInfo {
 	
+	@ApiModelProperty(value = "번호 (PK)")
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private Long id;
 
+	@ApiModelProperty(value = "제목")
 	@Column(name = "title")
 	private String title;
 	
+	@ApiModelProperty(value = "내용")
 	@Column(name = "description")
 	private String description;
 	
+	@ApiModelProperty(value = "생성일")
 	@Column(name = "created")
 	@CreationTimestamp
 	private Date created;
