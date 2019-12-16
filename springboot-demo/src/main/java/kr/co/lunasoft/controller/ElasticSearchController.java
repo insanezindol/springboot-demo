23da5cf6bdf9fa243e223e1cdea11e6ccde0dae9
딘 <jhlee@lunasoft.co.kr>
2019-12-16 12:52:52 +0900

    modify elastic search

--- springboot-demo/src/main/java/kr/co/lunasoft/controller/ElasticSearchController.java
+++ springboot-demo/src/main/java/kr/co/lunasoft/controller/ElasticSearchController.java
@@ -35,18 +35,18 @@ public class ElasticSearchController {
 	@Autowired
 	ElasticApi elasticApi;
 
-	@ApiOperation("엘라스틱서치 GET 전송")
+	@ApiOperation("ElasticSearch GET - 조회")
 	@ApiImplicitParams({ 
 		@ApiImplicitParam(name = "index", value = "조회할 index 값", required = true, dataType = "string", paramType = "path", defaultValue = "lunasoft"), 
 		@ApiImplicitParam(name = "type", value = "조회할 type 값", required = true, dataType = "string", paramType = "path", defaultValue = "dean"), 
 		@ApiImplicitParam(name = "id", value = "조회할 id 값", required = true, dataType = "string", paramType = "path", defaultValue = "1"), 
 		})
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "Success", response = ResponseInfo.class) })
-	@GetMapping(value = "/resource/{index}/{type}/{id}")
+	@GetMapping(value = "/{index}/{type}/{id}")
 	public JSONObject getResource(@PathVariable String index, @PathVariable String type, @PathVariable String id) {
 		String url = index + "/" + type + "/" + id;
 
-		Map<String, Object> result = elasticApi.callElasticApi("GET", url, null, null);
+		Map<String, Object> result = elasticApi.callElasticApi("GET", url, null);
 
 		JSONObject obj = new JSONObject();
 		obj.put("code", "100200");
@@ -55,17 +55,17 @@ public class ElasticSearchController {
 		return obj;
 	}
 
-	@ApiOperation("엘라스틱서치 POST 전송 (id없이 전송)")
+	@ApiOperation("ElasticSearch POST - key 없이 사용자 정보 전송 (insert)")
 	@ApiImplicitParams({ 
 		@ApiImplicitParam(name = "index", value = "조회할 index 값", required = true, dataType = "string", paramType = "path", defaultValue = "lunasoft"), 
 		@ApiImplicitParam(name = "type", value = "조회할 type 값", required = true, dataType = "string", paramType = "path", defaultValue = "dean"), 
 		})
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "Success", response = ResponseInfo.class) })
-	@PostMapping(value = "/resource/user/{index}/{type}")
+	@PostMapping(value = "/user/{index}/{type}")
 	public JSONObject postUserInfoResource(@PathVariable String index, @PathVariable String type, @ApiParam(name = "userInfo", value = "추가할 사용자 정보", required = true) @RequestBody UserInfo userInfo) {
 		String url = index + "/" + type;
 
-		Map<String, Object> result = elasticApi.callElasticApi("POST", url, userInfo, null);
+		Map<String, Object> result = elasticApi.callElasticApi("POST", url, userInfo);
 
 		JSONObject obj = new JSONObject();
 		obj.put("code", "100200");
@@ -74,17 +74,17 @@ public class ElasticSearchController {
 		return obj;
 	}
 	
-	@ApiOperation("엘라스틱서치 POST 전송 (id없이 전송)")
+	@ApiOperation("ElasticSearch POST - key 없이 부서 정보 전송 (insert)")
 	@ApiImplicitParams({ 
-		@ApiImplicitParam(name = "index", value = "조회할 index 값", required = true, dataType = "string", paramType = "path", defaultValue = "lunasoft"), 
-		@ApiImplicitParam(name = "type", value = "조회할 type 값", required = true, dataType = "string", paramType = "path", defaultValue = "dean"), 
+		@ApiImplicitParam(name = "index", value = "조회할 index 값", required = true, dataType = "string", paramType = "path", defaultValue = "lunasoft-sub1"), 
+		@ApiImplicitParam(name = "type", value = "조회할 type 값", required = true, dataType = "string", paramType = "path", defaultValue = "dept"), 
 		})
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "Success", response = ResponseInfo.class) })
-	@PostMapping(value = "/resource/dept/{index}/{type}")
+	@PostMapping(value = "/dept/{index}/{type}")
 	public JSONObject postDeptInfoResource(@PathVariable String index, @PathVariable String type, @ApiParam(name = "deptInfo", value = "추가할 부서 정보", required = true) @RequestBody DeptInfo deptInfo) {
 		String url = index + "/" + type;
 
-		Map<String, Object> result = elasticApi.callElasticApi("POST", url, deptInfo, null);
+		Map<String, Object> result = elasticApi.callElasticApi("POST", url, deptInfo);
 
 		JSONObject obj = new JSONObject();
 		obj.put("code", "100200");
@@ -93,18 +93,18 @@ public class ElasticSearchController {
 		return obj;
 	}
 
-	@ApiOperation("엘라스틱서치 PUT 전송")
+	@ApiOperation("ElasticSearch PUT - key 함께 사용자 정보 전송 (upsert)")
 	@ApiImplicitParams({ 
 		@ApiImplicitParam(name = "index", value = "조회할 index 값", required = true, dataType = "string", paramType = "path", defaultValue = "lunasoft"), 
 		@ApiImplicitParam(name = "type", value = "조회할 type 값", required = true, dataType = "string", paramType = "path", defaultValue = "dean"), 
 		@ApiImplicitParam(name = "id", value = "조회할 id 값", required = true, dataType = "string", paramType = "path", defaultValue = "1"), 
 		})
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "Success", response = ResponseInfo.class) })
-	@PutMapping(value = "/resource/user/{index}/{type}/{id}")
+	@PutMapping(value = "/user/{index}/{type}/{id}")
 	public JSONObject putResource(@PathVariable String index, @PathVariable String type, @PathVariable String id, @ApiParam(name = "userInfo", value = "추가할 사용자 정보", required = true) @RequestBody UserInfo userInfo) {
 		String url = index + "/" + type + "/" + id;
 
-		Map<String, Object> result = elasticApi.callElasticApi("PUT", url, userInfo, null);
+		Map<String, Object> result = elasticApi.callElasticApi("PUT", url, userInfo);
 
 		JSONObject obj = new JSONObject();
 		obj.put("code", "100200");
@@ -113,18 +113,18 @@ public class ElasticSearchController {
 		return obj;
 	}
 
-	@ApiOperation("엘라스틱서치 DELETE 전송")
+	@ApiOperation("ElasticSearch DELETE - index, type, id로 해당 정보 삭제")
 	@ApiImplicitParams({ 
 		@ApiImplicitParam(name = "index", value = "조회할 index 값", required = true, dataType = "string", paramType = "path", defaultValue = "lunasoft"), 
 		@ApiImplicitParam(name = "type", value = "조회할 type 값", required = true, dataType = "string", paramType = "path", defaultValue = "dean"), 
 		@ApiImplicitParam(name = "id", value = "조회할 id 값", required = true, dataType = "string", paramType = "path", defaultValue = "1"), 
 		})
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "Success", response = ResponseInfo.class) })
-	@DeleteMapping(value = "/resource/{index}/{type}/{id}")
+	@DeleteMapping(value = "/{index}/{type}/{id}")
 	public JSONObject deleteResource(@PathVariable String index, @PathVariable String type, @PathVariable String id) {
 		String url = index + "/" + type + "/" + id;
 
-		Map<String, Object> result = elasticApi.callElasticApi("DELETE", url, null, null);
+		Map<String, Object> result = elasticApi.callElasticApi("DELETE", url, null);
 
 		JSONObject obj = new JSONObject();
 		obj.put("code", "100200");
--- springboot-demo/src/main/java/kr/co/lunasoft/util/ElasticApi.java
+++ springboot-demo/src/main/java/kr/co/lunasoft/util/ElasticApi.java
@@ -1,23 +1,20 @@
 package kr.co.lunasoft.util;
 
-import com.google.gson.Gson;
-import org.apache.http.Header;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
 import org.apache.http.entity.ContentType;
-import org.apache.http.message.BasicHeader;
 import org.apache.http.nio.entity.NStringEntity;
 import org.apache.http.util.EntityUtils;
+import org.elasticsearch.client.Request;
 import org.elasticsearch.client.Response;
 import org.elasticsearch.client.RestClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.elasticsearch.client.Request;
 
-import java.util.Base64;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import com.google.gson.Gson;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -26,56 +23,42 @@ import lombok.extern.slf4j.Slf4j;
 public class ElasticApi {
 
 	@Value("${elasticsearch.host}")
-	private String host;
+	private String HOST;
 
 	@Value("${elasticsearch.port}")
-	private int port;
-	
-	@Value("${elasticsearch.user}")
-	private String user;
-	
-	@Value("${elasticsearch.password}")
-	private String password;
+	private int PORT;
 
 	/**
-	 * 엘라스틱서치에서 제공하는 api를 이용한 전송메소드
+	 * 엘라스틱서치에서 제공하는 api를 이용한 전송 메소드 (동기)
 	 * 
 	 * @param method
 	 * @param url
 	 * @param obj
-	 * @param jsonData
 	 * @return
 	 */
-	public Map<String, Object> callElasticApi(String method, String url, Object obj, String jsonData) {
+	public Map<String, Object> callElasticApi(String method, String url, Object obj) {
 		Map<String, Object> result = new HashMap<>();
 
-		String jsonString;
-		// json형태의 파라미터가 아니라면 gson으로 만들어주자.
-		if (jsonData == null) {
-			Gson gson = new Gson();
-			jsonString = gson.toJson(obj);
-		} else {
-			jsonString = jsonData;
-		}
+		// object to string
+		Gson gson = new Gson();
+		String jsonString = gson.toJson(obj);
 
-		// 엘라스틱서치에서 제공하는 restClient를 통해 엘라스틱서치에 접속한다
-		try (RestClient restClient = RestClient.builder(new HttpHost(host, port)).build()) {
-			// 엘라스틱서치에서 제공하는 response 객체
-			Response response = null;
+		RestClient restClient = null;
+		try {
+			restClient = RestClient.builder(new HttpHost(HOST, PORT, "http")).build();
 
-			// GET, DELETE 메소드는 HttpEntity가 필요없다
+			Request request = new Request(method, url);
+
 			if (method.equals("GET") || method.equals("DELETE")) {
-				Request request = new Request(method, url);
-				response = restClient.performRequest(request);
+				// GET, DELETE 메소드는 HttpEntity가 필요없다
 			} else {
 				HttpEntity entity = new NStringEntity(jsonString, ContentType.APPLICATION_JSON);
-				Request request = new Request(method, url);
 				request.setEntity(entity);
-				response = restClient.performRequest(request);
 			}
-			// 앨라스틱서치에서 리턴되는 응답코드를 받는다
+
+			Response response = restClient.performRequest(request);
+
 			int statusCode = response.getStatusLine().getStatusCode();
-			// 엘라스틱서치에서 리턴되는 응답메시지를 받는다
 			String responseBody = EntityUtils.toString(response.getEntity());
 
 			log.info("[" + statusCode + "] : " + responseBody);
@@ -85,55 +68,16 @@ public class ElasticApi {
 		} catch (Exception e) {
 			result.put("resultCode", -1);
 			result.put("resultBody", e.toString());
+			log.error("[E02] " + e.toString());
+		} finally {
+			try {
+				restClient.close();
+			} catch (Exception e) {
+				log.error("[E03] " + e.toString());
+			}
 		}
+
 		return result;
 	}
-	
-	public Map<String, Object> callElasticApiAuth(String method, String url, Object obj, String jsonData) {
-        Map<String, Object> result = new HashMap<>();
-
-        String jsonString;
-        //json형태의 파라미터가 아니라면 gson으로 만들어주자.
-        if (jsonData == null) {
-            Gson gson = new Gson();
-            jsonString = gson.toJson(obj);
-        } else {
-            jsonString = jsonData;
-        }
-
-        //엘라스틱서치에서 제공하는 restClient를 통해 엘라스틱서치에 접속한다
-        try(RestClient restClient = RestClient.builder(new HttpHost(host, port)).build()) {
-            String auth = user+":"+password;
-            String basicAuth  = "Basic "+ Base64.getEncoder().encodeToString(auth.getBytes());
-            Header[] headers = {
-                    new BasicHeader("Authorization", basicAuth)
-            };
-            Map<String, String> params =  Collections.singletonMap("pretty", "true");
-            //엘라스틱서치에서 제공하는 response 객체
-            Response response = null;
-
-            //GET, DELETE 메소드는 HttpEntity가 필요없다
-            if (method.equals("GET") || method.equals("DELETE")) {
-				Request request = new Request(method, url);
-				response = restClient.performRequest(request);
-			} else {
-				HttpEntity entity = new NStringEntity(jsonString, ContentType.APPLICATION_JSON);
-				Request request = new Request(method, url);
-				request.setEntity(entity);
-				response = restClient.performRequest(request);
-			}
-            
-            //앨라스틱서치에서 리턴되는 응답코드를 받는다
-            int statusCode = response.getStatusLine().getStatusCode();
-            //엘라스틱서치에서 리턴되는 응답메시지를 받는다
-            String responseBody = EntityUtils.toString(response.getEntity());
-            result.put("resultCode", statusCode);
-            result.put("resultBody", responseBody);
-        } catch (Exception e) {
-            result.put("resultCode", -1);
-            result.put("resultBody", e.toString());
-        }
-        return result;
-    }
 
 }
