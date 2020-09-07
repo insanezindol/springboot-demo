67289d882521645e11ee03f27d90f4ea1824cd4c
dean <jhlee@lunasoft.co.kr>
2020-09-07 15:39:42 +0900

    add mongodb regdate upddate

--- springboot-demo/src/main/java/kr/co/lunasoft/model/HumanInfo.java
+++ springboot-demo/src/main/java/kr/co/lunasoft/model/HumanInfo.java
@@ -4,20 +4,28 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
-@ApiModel(value="HumanInfo", description="MongoDB Human Collection Object")
+import java.util.Date;
+
+@ApiModel(value = "HumanInfo", description = "MongoDB Human Collection Object")
 @Data
 public class HumanInfo {
-	
-	@ApiModelProperty(value = "아이디")
-	private String id;
-	
-	@ApiModelProperty(value = "이름")
-	private String name;
-    
-	@ApiModelProperty(value = "성별 - 0 : 남자 , 1 : 여자")
-	private String gender;
-	
-	@ApiModelProperty(value = "부양가족")
-	private ChildInfo childInfo;
-	
+
+    @ApiModelProperty(value = "아이디")
+    private String id;
+
+    @ApiModelProperty(value = "이름")
+    private String name;
+
+    @ApiModelProperty(value = "성별 - 0 : 남자 , 1 : 여자")
+    private String gender;
+
+    @ApiModelProperty(value = "부양가족")
+    private ChildInfo childInfo;
+
+    @ApiModelProperty(value = "등록시간")
+    private Date regDate;
+
+    @ApiModelProperty(value = "수정시간")
+    private Date updDate;
+
 }
--- springboot-demo/src/main/java/kr/co/lunasoft/service/MongoService.java
+++ springboot-demo/src/main/java/kr/co/lunasoft/service/MongoService.java
@@ -1,7 +1,7 @@
 package kr.co.lunasoft.service;
 
-import java.util.List;
-
+import kr.co.lunasoft.model.HumanInfo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -9,32 +9,46 @@ import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
-import kr.co.lunasoft.model.HumanInfo;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.Date;
+import java.util.List;
 
+@Slf4j
 @Service
 public class MongoService {
 
-	@Autowired
-	MongoTemplate mongoTemplate;
+    @Autowired
+    MongoTemplate mongoTemplate;
 
-	public List<HumanInfo> getList() {
-		return mongoTemplate.findAll(HumanInfo.class, "human");
-	}
+    public List<HumanInfo> getList() {
+        return mongoTemplate.findAll(HumanInfo.class, "human");
+    }
 
-	public HumanInfo getOne(String name) {
-		return mongoTemplate.findOne(new Query(Criteria.where("name").is(name)), HumanInfo.class, "human");
-	}
+    public HumanInfo getOne(String name) {
+        return mongoTemplate.findOne(new Query(Criteria.where("name").is(name)), HumanInfo.class, "human");
+    }
 
-	public void add(HumanInfo param) {
-		mongoTemplate.save(param, "human");
-	}
+    public void add(HumanInfo param) {
+        Date nowDate = new Date();
+        param.setRegDate(nowDate);
+        param.setUpdDate(nowDate);
+        mongoTemplate.save(param, "human");
+    }
 
-	public void modify(HumanInfo param) {
-		mongoTemplate.findAndModify(new Query(Criteria.where("_id").is(param.getId())), new Update().set("name", param.getName()).set("gender", param.getGender()), HumanInfo.class, "human");
-	}
+    public void modify(HumanInfo param) {
+        mongoTemplate.findAndModify(
+                new Query(
+                        Criteria.where("_id").is(param.getId())),
+                new Update()
+                        .set("name", param.getName())
+                        .set("gender", param.getGender())
+                        .set("updDate", new Date())
+                , HumanInfo.class, "human");
+    }
 
-	public void delete(String id) {
-		mongoTemplate.remove(new Query(Criteria.where("_id").is(id)), HumanInfo.class, "human");
-	}
+    public void delete(String id) {
+        mongoTemplate.remove(new Query(Criteria.where("_id").is(id)), HumanInfo.class, "human");
+    }
 
 }
